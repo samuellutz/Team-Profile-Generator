488727996ca151bb55fac44d048dd0b5f09de598
samuel lutz <samlutz89@gmail.com>
2022-01-24 17:00:28 -0800

    inital input for manager working

--- index.js
+++ index.js
@@ -10,7 +10,34 @@ const questions = () => {
             name: 'name',
             message: 'What is your Managers name?'
         },
-
+        {
+            type: 'input',
+            name: 'ID',
+            message: 'What is your team Managers ID?'
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: 'What is your Managers office number?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your Managers Email?'
+        },
+        {
+            type: 'list',
+            name: 'addMemeber',
+            message: 'what type of team member would you like to add next?',
+            choices: ['Engineer', 'Intern', 'I don\'t want to add any more team members'],
+        }
     ])
-}
+    .then((manager) => {
+        switch(manager.addMember) {
+            case 'Engineer':
+                engineerQuestions();
+                break;
+        }
+    })
+};
 questions();
